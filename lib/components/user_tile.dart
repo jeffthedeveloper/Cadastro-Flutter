d131e4fcc7ab2e01e73db66eaebf740ffb2c237c
jeffthedeveloper <jeffersonfir@gmail.com>
2025-03-04 09:16:29 -0300

    iniciando na manipulação do C.R.U.D aplicando o DELETE - módulo para exclusão de usuários

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/models/user.dart';
+import 'package:flutter_crud/provider/users.dart';
 import 'package:flutter_crud/routes/app_routes.dart';
+import 'package:provider/provider.dart';
 
 class UserTile extends StatelessWidget {
   final User? user;
@@ -16,30 +18,53 @@ class UserTile extends StatelessWidget {
         ? const CircleAvatar(child: Icon(Icons.person))
         : CircleAvatar(backgroundImage: NetworkImage(user!.avatarUrl));
     return ListTile(
-        leading: avatar,
-        title: Text(user!.name),
-        subtitle: Text(user!.email),
-        trailing: SizedBox(
-          width: 100,
-          child: Row(
-            children: [
-              IconButton(
-                icon: const Icon(Icons.edit),
-                color: Colors.orange,
-                onPressed: () {
-                  Navigator.of(context).pushNamed(
-                    AppRoutes.USER_FORM,
-                    arguments: user,
-                  );
-                },
-              ),
-              IconButton(
-                icon: const Icon(Icons.delete),
-                color: Colors.red,
-                onPressed: () {},
-              ),
-            ],
-          ),
-        ));
+      leading: avatar,
+      title: Text(user!.name),
+      subtitle: Text(user!.email),
+      trailing: SizedBox(
+        width: 100,
+        child: Row(
+          children: [
+            IconButton(
+              icon: const Icon(Icons.edit),
+              color: Colors.orange,
+              onPressed: () {
+                Navigator.of(context).pushNamed(
+                  AppRoutes.USER_FORM,
+                  arguments: user,
+                );
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.delete),
+              color: Colors.red,
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: Text('Excluir Usuário'),
+                    content: Text('Tem certeza???'),
+                    actions: [
+                      TextButton(
+                        child: Text('Não'),
+                        onPressed: () => Navigator.of(context).pop(false),
+                      ),
+                      TextButton(
+                        child: Text('Sim'),
+                        onPressed: () => Navigator.of(context).pop(true),
+                      ),
+                    ],
+                  ),
+                ).then((confirmed) {
+                  if (confirmed) {
+                    Provider.of<Users>(context, listen: false).remove(user!);
+                  }
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
